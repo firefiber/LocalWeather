5867bd22fd60936624f2d7073507ca3f472b8875
Shan W <shan.w@outlook.com>
2018-01-03 23:43:50 +0400

    test

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 ////OpenWeatherMap API KEY 5b72d586dd7fd8a9036a04859fa2a7c7
 //
-////var url = "https://samples.openweathermap.org/data/2.5/weather?appid=b1b15e88fa797225412429c1c50c122a1&q=London,uk"
-////var newAJAX = new XMLHttpRequest();
-////
-////newAJAX.open('GET', url);
-////newAJAX.send();
-////newAJAX.responseType = 'json';
-////
-////newAJAX.onload = function(){
-////  responseObj = newAJAX.response;
-////  console.log(responseObj);
-////}
-//
+  var url = "https://api.openweathermap.org/data/2.5/weather?q=London&APPID=5b72d586dd7fd8a9036a04859fa2a7c7"
+  var newAJAX = new XMLHttpRequest();
+
+  newAJAX.open('GET', url);
+  newAJAX.send();
+  newAJAX.responseType = 'json';
+
+  newAJAX.onload = function(){
+    alert("hello")
+    responseObj = newAJAX.response;
+    showWeather(responseObj);
+  }
+
 //$.ajax({
 //  url: "https://api.openweathermap.org/data/2.5/weather",
+//  jsonp: false,
 //  data:{
 //    "q": "London",
 //    "APPID": "5b72d586dd7fd8a9036a04859fa2a7c7",
@@ -22,6 +24,18 @@
 //});
 
 function showWeather(data){
-  console.log(data);
-  alert("hello");
+  console.log(JSON.parse(data));
+  alert("test");
 }
+//
+////(function(){
+////  jsonp();
+////  document.querySelector('button').onclick = function(){
+////    jsonp();
+////  }
+////})();
+//
+//(function(){
+//  jsonp();
+//});
+
