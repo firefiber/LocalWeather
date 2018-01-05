f0562420783db02f0e0a0892057016aa29921200
Shan W <shan.w@outlook.com>
2018-01-06 01:04:13 +0400

    Got geolocation working on vanilla JS.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
 ////OpenWeatherMap API KEY 5b72d586dd7fd8a9036a04859fa2a7c7
 //
-  var url = "https://api.openweathermap.org/data/2.5/weather?q=London&APPID=5b72d586dd7fd8a9036a04859fa2a7c7"
+  navigator.geolocation.getCurrentPosition(success);
+  function success(position){
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    alert(lat);
+    alert(lon);
+
+  var url = "https://api.openweathermap.org/data/2.5/weather?&units=metric&APPID=5b72d586dd7fd8a9036a04859fa2a7c7"
   var newAJAX = new XMLHttpRequest();
 
-  newAJAX.open('GET', url);
+  newAJAX.open('GET', url+"&"+"lat="+lat+"&lon="+lon);
   newAJAX.send();
-  newAJAX.responseType = 'json';
+  newAJAX.responseType = 'text';
 
   newAJAX.onload = function(){
     alert("hello")
     responseObj = newAJAX.response;
     showWeather(responseObj);
   }
-
+  }
 //$.ajax({
 //  url: "https://api.openweathermap.org/data/2.5/weather",
 //  jsonp: false,
@@ -25,6 +32,7 @@
 
 function showWeather(data){
   console.log(JSON.parse(data));
+//  console.log(data);
   alert("test");
 }
 //
