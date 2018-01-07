ef4b13ab0a8f14c2e36d76d63c80f6d40f9cbfd7
Shan W <shan.w@outlook.com>
2018-01-08 00:50:47 +0400

    Page now displays user location and temperature upon acceptance of location search.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 ////OpenWeatherMap API KEY 5b72d586dd7fd8a9036a04859fa2a7c7
 //
+function json(){
   navigator.geolocation.getCurrentPosition(success);
   function success(position){
     var lat = position.coords.latitude;
@@ -7,19 +8,19 @@
     alert(lat);
     alert(lon);
 
-  var url = "https://api.openweathermap.org/data/2.5/weather?&units=metric&APPID=5b72d586dd7fd8a9036a04859fa2a7c7"
-  var newAJAX = new XMLHttpRequest();
+    var url = "https://api.openweathermap.org/data/2.5/weather?&units=metric&APPID=5b72d586dd7fd8a9036a04859fa2a7c7"
+    var newAJAX = new XMLHttpRequest();
 
-  newAJAX.open('GET', url+"&"+"lat="+lat+"&lon="+lon);
-  newAJAX.send();
-  newAJAX.responseType = 'text';
+    newAJAX.open('GET', url+"&"+"lat="+lat+"&lon="+lon);
+    newAJAX.send();
+    newAJAX.responseType = 'text';
 
-  newAJAX.onload = function(){
-    alert("hello")
-    responseObj = newAJAX.response;
-    showWeather(responseObj);
+    newAJAX.onload = function(){
+      responseObj = newAJAX.response;
+      showWeather(responseObj);
+    }
   }
-  }
+}
 //$.ajax({
 //  url: "https://api.openweathermap.org/data/2.5/weather",
 //  jsonp: false,
@@ -31,9 +32,14 @@
 //});
 
 function showWeather(data){
-  console.log(JSON.parse(data));
-//  console.log(data);
-  alert("test");
+  var parseData = JSON.parse(data);
+  var country = document.createElement('p');
+  var temp = document.createElement('p');
+  country.textContent = parseData.name;
+  temp.textContent = parseData.main.temp;
+  var div = document.getElementById("weatherStats");
+  div.appendChild(country);
+  div.appendChild(temp);
 }
 //
 ////(function(){
@@ -43,7 +49,7 @@ function showWeather(data){
 ////  }
 ////})();
 //
-//(function(){
-//  jsonp();
-//});
+(function(){
+  json();
+})();
 
