c898e1bbe573def45fef34c6cfc484f8f1584b69
Shan W <shan.w@outlook.com>
2018-01-10 11:04:39 +0400

    Added Google Maps reverse geocode for better location names.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 ////OpenWeatherMap API KEY 5b72d586dd7fd8a9036a04859fa2a7c7
+////Google API Key AIzaSyAKue3iOUaEZzcFkBk61UV7kTv1qSNGwlI
 
 function json(){
   navigator.geolocation.getCurrentPosition(success);
@@ -8,28 +9,48 @@ function json(){
     alert(lat);
     alert(lon);
 
-    var url = "https://api.openweathermap.org/data/2.5/weather?&units=metric&APPID=5b72d586dd7fd8a9036a04859fa2a7c7"
-    var newAJAX = new XMLHttpRequest();
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&APPID=5b72d586dd7fd8a9036a04859fa2a7c7"
+    var gmapsURL = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAKue3iOUaEZzcFkBk61UV7kTv1qSNGwlI"
 
-    newAJAX.open('GET', url+"&"+"lat="+lat+"&lon="+lon);
-    newAJAX.send();
-    newAJAX.responseType = 'text';
+    //Weather Request
+    var weatherRequest = new XMLHttpRequest();
 
-    newAJAX.onload = function(){
-      responseObj = newAJAX.response;
+    weatherRequest.open('GET', weatherURL+"&"+"lat="+lat+"&lon="+lon);
+    weatherRequest.send();
+    weatherRequest.responseType = 'text';
+
+    weatherRequest.onload = function(){
+      responseObj = weatherRequest.response;
       showWeather(responseObj);
+    }
+
+    //Google Location Request
+    var mapsRequest = new XMLHttpRequest();
+
+    mapsRequest.open('GET', gmapsURL+"&latlng="+lat+","+lon);
+    mapsRequest.send();
+    mapsRequest.responseType = 'json';
+
+    mapsRequest.onload = function(){
+      responseObj = mapsRequest.response;
+      console.log(responseObj);
+      showLocation(responseObj);
+      alert(responseObj.results[0].formatted_address)
     }
   }
 }
 
-function showWeather(data){
-  var parseData = JSON.parse(data);
+function showLocation(data){
   var country = document.createElement('p');
-  var temp = document.createElement('p');
-  country.textContent = parseData.name;
-  temp.textContent = parseData.main.temp;
+  country.textContent = responseObj.results[0].formatted_address;
   var div = document.getElementById("weatherStats");
   div.appendChild(country);
+}
+function showWeather(data){
+  var parseData = JSON.parse(data);
+  var temp = document.createElement('p');
+  temp.textContent = parseData.main.temp;
+  var div = document.getElementById("weatherStats");
   div.appendChild(temp);
 }
 
